3136d354929b10c6d133b7978d127ff3d9df9feb
Yixin88 <yixin88@hotmail.co.uk>
2022-12-01 01:04:12 +0000

    added the first 3 project

--- script.js
+++ script.js
@@ -13,3 +13,21 @@ const newProjectCard = (projectTitle, imageName, description, projectLink, codeL
                            </div>`;
     projectContainer.appendChild(container);
 }
+
+newProjectCard("The Bean Machine", 
+                "coffee_landing_page", 
+                "Coffee shop landing page including selction page, with the use of both flexbox and grid. Also animations and pseudo classes.", 
+                "https://yixin88.github.io/Landing-page-coffee-/", 
+                "https://github.com/Yixin88/Landing-page-coffee-");
+
+newProjectCard("Ski Adventure Form", 
+                "ski_form", 
+                "Sign-up form for a Ski resort, with the use of input validation (e.g. Empty/invalid/improper format field) and use of all common input types.", 
+                "https://yixin88.github.io/Ski-Adventure-Form/", 
+                "https://github.com/Yixin88/Ski-Adventure-Form");
+
+newProjectCard("Currency Exchange", 
+                "currency", 
+                "A web application that fetch latest currency exchange rate via API depending on the 2 currency you wish to exchange between.", 
+                "https://yixin88.github.io/Currency-Exchange-App/", 
+                "https://github.com/Yixin88/Currency-Exchange-App");
