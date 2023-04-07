787bb4e8f0bb98468537c664cef1b6bc523570f5
Yixin <yixin88@hotmail.co.uk>
2023-04-07 15:35:47 +0100

    fixed styling error for header nav, now its responsive

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const menu_btn = document.querySelector('.hamburger');
+const mobile_btn = document.querySelector('.mobile-nav');
 
 menu_btn.addEventListener('click', ()=> {
-     menu_btn.classList.toggle('is-active')
+     menu_btn.classList.toggle('is-active');
+     mobile_btn.classList.toggle('is-active');
 })
 
 
